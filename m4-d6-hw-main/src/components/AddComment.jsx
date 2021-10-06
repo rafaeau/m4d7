14a18a0a3d453370e6c8da3e1e39078a9eb609dc
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-10-06 13:33:57 +0200

    AddComment converted

--- m4-d6-hw-main/src/components/AddComment.jsx
+++ m4-d6-hw-main/src/components/AddComment.jsx
@@ -1,33 +1,47 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Button, Form } from 'react-bootstrap'
 
-class AddComment extends Component {
+const AddComment = ({asin}) => {
 
-    state = {
+    /* state = {
         comment: {
             comment: '',
             rate: 1,
             elementId: null
         }
-    }
+    } */
 
-    componentDidUpdate(prevProps) {
+    const [comment, setComment] = useState({
+        comment: '',
+        rate: 1,
+        elementId: null  
+    })
+
+
+   /*  componentDidUpdate(prevProps) {
         if (prevProps.asin !== this.props.asin) {
             this.setState({
                 comment: {
-                    ...this.state.comment,
+                    ...comment,
                     elementId: this.props.asin
                 }
             })
         }
-    }
+    } */
 
-    sendComment = async (e) => {
+    useEffect (() => {
+        setComment(c => ({
+            ...c,
+            elementId: asin
+        }))
+    }, [asin])
+
+    const sendComment = async (e) => {
         e.preventDefault()
         try {
             let response = await fetch('https://striveschool-api.herokuapp.com/api/comments', {
                 method: 'POST',
-                body: JSON.stringify(this.state.comment),
+                body: JSON.stringify(comment),
                 headers: {
                     'Content-type': 'application/json',
                     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2MzM1MDIzNTIsImV4cCI6MTYzNDcxMTk1Mn0.Q122L_go7ccEBUNjAxhnbaBOwUGsO5DhEmvxHEP6obc'
@@ -45,33 +59,30 @@ class AddComment extends Component {
         }
     }
 
-    render() {
         return (
             <div>
-                <Form onSubmit={this.sendComment}>
+                <Form onSubmit={sendComment}>
                     <Form.Group>
                         <Form.Label>Comment text</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Add comment here"
-                            value={this.state.comment.comment}
-                            onChange={e => this.setState({
-                                comment: {
-                                    ...this.state.comment,
+                            value={comment.comment}
+                            onChange={e => setComment({
+                                ...comment,
                                     comment: e.target.value
-                                }
-                            })}
+                                })
+                            }
                         />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Rating</Form.Label>
-                        <Form.Control as="select" value={this.state.comment.rate}
-                            onChange={e => this.setState({
-                                comment: {
-                                    ...this.state.comment,
+                        <Form.Control as="select" value={comment.rate}
+                            onChange={e => setComment({
+                                ...comment,
                                     rate: e.target.value
-                                }
-                            })}>
+                                })
+                            }>
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>
@@ -86,6 +97,6 @@ class AddComment extends Component {
             </div>
         )
     }
-}
+
 
 export default AddComment
