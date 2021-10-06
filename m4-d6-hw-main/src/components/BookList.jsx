de84da4cce1326f3615e4924dc865d404022a7ff
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-10-06 11:46:45 +0200

    BookList converted

--- m4-d6-hw-main/src/components/BookList.jsx
+++ m4-d6-hw-main/src/components/BookList.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import {useState} from 'react'
 import SingleBook from './SingleBook'
 import { Col, Container, Form, Row } from 'react-bootstrap'
 import CommentArea from './CommentArea'
 
-class BookList extends React.Component {
+const BookList = ({books}) => {
 
-    state = {
+    const [searchQuery, setSearchQuery] = useState('')
+    const [selectedBook, setSelectedBook] = useState (null)
+
+    /* state = {
         searchQuery: '',
         selectedBook: null
-    }
+    } */
 
-    render() {
         return (
             <Container>
                 <Row>
@@ -22,35 +24,32 @@ class BookList extends React.Component {
                                     <Form.Control
                                         type="text"
                                         placeholder="Search here"
-                                        value={this.state.searchQuery}
-                                        onChange={e => this.setState({ searchQuery: e.target.value })}
+                                        value={searchQuery}
+                                        onChange={e => setSearchQuery(e.target.value )}
                                     />
                                 </Form.Group>
                             </Col>
                         </Row>
                         <Row>
                             {
-                                this.props.books.filter(b => b.title.toLowerCase().includes(this.state.searchQuery)).map(b => (
+                                books.filter(b => b.title.toLowerCase().includes(searchQuery)).map(b => (
                                     <Col xs={3} key={b.asin} >
                                         <SingleBook
                                             book={b}
-                                            selectedBook={this.state.selectedBook}
-                                            changeSelectedBook={asin => this.setState({
-                                                selectedBook: asin
-                                            })} />
+                                            selectedBook={selectedBook}
+                                            changeSelectedBook={asin => setSelectedBook (asin)} />
                                     </Col>
                                 ))
                             }
                         </Row>
                     </Col>
                     <Col md={4}>
-                        <CommentArea asin={this.state.selectedBook} />
+                        <CommentArea asin={selectedBook} />
                     </Col>
                 </Row>
             </Container>
         )
     }
 
-}
 
 export default BookList
